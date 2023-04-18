f21c3d491994ffd10e4ec1844d580d745ca97f23
Soribel SB <soribelsantos0514@gmail.com>
2023-04-18 00:19:00 -0400

    feat validacion campos fullname y mensaje

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,12 @@
+//TYPE JS TITULO
+// Setup and start animation!
+let bannerTitulo = document.querySelector(".banner-tittle");
+let typed = new Typed(bannerTitulo,{
+  strings: ['FullStack <br> Developer'],
+  typeSpeed: 45,
+  showCursor: false
+  });
+
 // INTERACTIVIDAD BOTONES ABRIR Y CERRAR MENU
 let menuBtnHamburguesa = document.getElementById("menu-btn-hamburguesa");
 let menuBtnCerrar = document.getElementById("menu-btn-close");
@@ -11,3 +20,42 @@ menuBtnCerrar.addEventListener("click", ()=> {
     menu.classList.remove("menu-visible");
     
 })
+
+// FORMULARIO VERIFICACION
+const btn = document.getElementById('btn-form');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    let fullname = document.getElementById('fullname');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message').value;
+    let warning = document.getElementById('warning');
+    let warningMessage = "";
+
+    const serviceID = 'service_xr97ydw';
+    const templateID = 'template_mghm9xt';
+    let valid = true;
+
+    if(fullname.value.endsWith("  ")) {
+      valid = false;
+      warning.textContent = `Too much space, on fullname.`;
+    } 
+    if(message.length < 35){
+      valid = false;
+      warning.innerHTML = `${warning.textContent}<br>The message is too short.`;
+    }
+    if(valid){
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+    }
+
+});
